5dc7ddeadf4c779303fb43dcae3583941c7ba13a
dhirajpidurkar <100358356+dhirajpidurkar@users.noreply.github.com>
2022-04-18 18:35:59 +0530

    Add files via upload

---
+++ Eureka-server/src/main/java/com/drp/SpringbootApp/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.drp.SpringbootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
